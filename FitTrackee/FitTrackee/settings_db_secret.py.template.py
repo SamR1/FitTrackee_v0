e671976a2d63d0c6aa6f7d22b7d20e758495ad59
Sam <sam@samr1.net>
2017-10-03 20:17:02 +0200

    setting update

---
+++ FitTrackee/FitTrackee/settings_db_secret.py.template.py
@@ -0,0 +1,16 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1 ',
+        'PORT': '3306',
+    }
+}
