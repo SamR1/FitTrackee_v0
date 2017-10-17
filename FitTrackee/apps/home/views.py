01074215967bf8f5bae1ca4edacef8f41488533f
SamR1 <samira.rabaaoui@coding-academy.fr>
2017-10-17 20:52:41 +0200

    User: Friends activities displayed in home page (WIP)

--- FitTrackee/apps/home/views.py
+++ FitTrackee/apps/home/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-#from django.template import RequestContext
+
+from ..activities.models import Activity
 
 
 def index(request):
-    return render(request, 'home/index.html')
+    friends_id = request.user.follows.all()
+    users_list = [user.id for user in friends_id if user in friends_id]
+    users_list.append(request.user.id)
+    activities = Activity.objects.order_by('-activity_date').filter(user_id__in=users_list)[:20]
+    return render(request, 'home/index.html', {'activities': activities})
